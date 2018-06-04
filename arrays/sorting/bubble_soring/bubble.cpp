db4fcb12f48f72e78118a446dea7bddd9f988227
Aditya Gupta <kryptobizz@gmail.com>
2018-06-04 22:26:41 +0530

    bubble cpp

---
+++ arrays/sorting/bubble_soring/bubble.cpp
@@ -0,0 +1,59 @@
+#include <cstdio>
+#include <iostream>
+#define MAX 10
+
+using namespace std;
+
+//function prototype
+void sortBubble(int arr[], int n);
+void swap(int *a, int *b);
+
+// main function
+int main() {
+    
+    int n;
+    int arr[MAX];
+    printf("Enter the array length. Maximum would be 10.\n");
+    cin >> n;
+
+    printf("Enter numbers...\n");
+
+    for (int i=0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    sortBubble(arr, n);
+    
+    printf("Sorted Array:\n");
+
+    for (int i=0; i < n; i++) {
+        printf("%d\t", arr[i]);
+    }
+    
+    return 0;
+}
+
+// function declaration
+void sortBubble(int arr[], int n){
+    
+    for(int i=0; i< n; i++) {
+        
+        for (int j=0; j < n; j++) {
+            if(arr[j] > arr[j+1] ) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+        
+    }
+    
+}
+
+
+// @todo
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
